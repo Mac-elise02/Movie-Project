e339d67f579757b91d0a310d5a0f7f2bba5e4169
Mac-elise02 <mac.elise2020@gmail.com>
2025-11-25 20:39:41 -0500

    Harry Potter themed movie projet

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ function closeMenu() {
   document.body.classList.remove("menu--open");
 }
 
-async function fetchMovies(potter) {
+async function fetchMovies(search = "potter") {
   const API_KEY = "50afa4e2";
-  const searchQuery = "potter"; 
 
-  const response = await fetch(`https://www.omdbapi.com/?i=${tt3896198}&apikey=50afa4e2&s=${potter}`); 
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${50afa4e2}&s=${potter}`); 
 
   const data = await response.json(); 
 
   if (!data.Search) return[]; 
 
   return data.Search.map(movie => ({
-    title: movie.title, 
+    Title: movie.Title, 
     year: movie.Year, 
     imdbID: movie.imdbID, 
     url: movie.Poster, 
@@ -29,13 +28,11 @@ let movies = null;
 let currentSearch = "potter"; 
 
 async function renderMovies(filter) {
-  const MovieWrapper = document.querySelector(".Movies"); 
+  const moviesWrapper = document.querySelector(".movies"); 
 
   if(!movies) {
     movies = await fetchMovies(currentSearch); 
   }
-
-  moviesWrapper.classList.remove('movies__loading'); 
 
   if(filter === "OLD_TO_NEW") {
     movies.sort((a, b) => Number(a.year) - Number(b.year)); 
@@ -45,18 +42,16 @@ async function renderMovies(filter) {
     movies.sort((a, b) => b.rating - a.rating); 
   }
 
-const moviesHtml = movies .map(movie => 
+const moviesHtml = movies 
+.map(
+  (movie) => 
   `<div class="movie">
      <figure class="movie__img--wrapper">
-       <div class="movie__img" src="${movie.url}"></div>
+       <img class="movie__img" src="${movie.url}" />
      </figure>
-     <div class="movie__title">
-       ${movie.title}
-     </div>
-     <div class="movie__rating">
-       ${ratingsHTML(movie.rating)}
-     </div>
-   <div>`
+     <div class="movie__title">${movie.Title}</div>
+     <div class="movie__rating">${ratingsHTML(movie.rating)}</div>
+   </div>`
 )
 .join(""); 
 
@@ -64,18 +59,14 @@ const moviesHtml = movies .map(movie =>
 }
 
 function ratingsHTML(rating) {
-  let ratingHTML = ""; 
+  let  ratingHTML= ""; 
   for(let i = 0; i < Math.floor(rating); ++i) {
-    ratingHTML += '<i class="fa-solid fa-star"></i>\n'; 
+    ratingHTML += '<i class="fa-solid fa-star"></i>'; 
   }
   if (!Number.isInteger(rating)) {
-    ratingHTML += '<i class="fa-solid fa-star-half-alt"></i>\n'; 
+    ratingHTML += '<i class="fa-solid fa-star-half-alt"></i>'; 
   }
   return ratingHTML; 
-}
-
-function filterMovies(event) {
-  renderMovies(event.target.value); 
 }
 
 let searchTimeout; 
@@ -86,73 +77,7 @@ function handleSearch(value) {
   searchTimeout = setTimeout(async () => {
     currentSearch = value.trim() || "potter"; 
 
-    movie = await fetchMovies(currentSearch); 
+    movies = await fetchMovies(currentSearch); 
     renderMovies(); 
   }, 400); 
-}
-
-// DATA API
-function getMovies() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([
-        {
-            Title: "Harry Potter and the Sorcerer's Stone",
-            Year: "2001", 
-            imbdID: "tt0241527", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BNTU1MzgyMDMtMzBlZS00YzczLThmYWEtMjU3YmFlOWEyMjE1XkEyXkFqcGc@._V1_SX300.jpg", 
-        },
-        {
-            Title: "Harry Potter and the Chamber of Secrets", 
-            Year: "2002", 
-            imbdID: "tt0295297", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BNGJhM2M2MWYtZjIzMC00MDZmLThkY2EtOWViMDhhYjRhMzk4XkEyXkFqcGc@._V1_SX300.jpg", 
-        }, 
-        {
-            Ttitle: "Harry Potter and the Prisoner of Azkaban", 
-            Year: "2004", 
-            imbdID: "tt0304141", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg", 
-        }, 
-        {
-            Title: "Harry Potter and the Goblet of Fire", 
-            Year: "2005", 
-            imbdID: "tt0330373", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_SX300.jpg", 
-        }, 
-        {
-            Title: "Harry Potter and the Order of the Phoenix", 
-            Year: "2007", 
-            imbdID: "tt0373889", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BYWJmM2M1YzItMjY1Ni00YzRmLTg5YWYtNDFmNTJjNzQ0ODkyXkEyXkFqcGc@._V1_SX300.jpg", 
-        }, 
-        {
-            Title: "Harry Potter and the Half-Blood Prince", 
-            Year: "2009", 
-            imbdID: "tt0417741", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_SX300.jpg",
-        }, 
-        {
-            Title: "Harry Potter and the Deathly Hallows(part 1)", 
-            Year: "2010", 
-            imbdID: "tt0926084", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BMTQ2OTE1Mjk0N15BMl5BanBnXkFtZTcwODE3MDAwNA@@._V1_SX300.jpg", 
-        }, 
-        {
-            Title: "Harry Potter and the Deathly Hallows(part 2)", 
-            Year: "2011", 
-            imbdID: "tt1201607", 
-            Type: "movie", 
-            Poster: "https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg", 
-        }
-      ]);
-    }, 1000);
-  });
 }
